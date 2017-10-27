a9c8a47b5e27d4bf20a3d67c0f132c16a9001ea7
Jose Melendez <Josem_melendez@icloud.com>
2017-10-27 19:27:38 -0400

    Added functions

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,27 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(slider.value)
+        startNewRound()
+        
+    }
+    
+    func updateLabels() {
+        
+        targetValueLabel.text = String(targetValue)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func startNewRound() {
+        
+        //Generate random number from 1-100
+        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        currentValue = 50
+        
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+        
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        
+        print("The value of the slider is now: \(slider.value)")
+        
+        currentValue = lroundf(slider.value)
     }
 
 
     @IBAction func showAlert() {
         
-       let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+        var difference: Int
+        
+        if currentValue > targetValue {
+            
+            difference = currentValue - targetValue
+            
+        } else if currentValue < targetValue {
+            
+            difference = targetValue - currentValue
+            
+        } else {
+            
+            difference = 0
+        }
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is \(targetValue)" + "\nThe difference is \(difference)"
+        
+       let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
 }
